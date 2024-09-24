8122b7ca4f11e3ad50a3a82401d63cc550fb1060
Admin <reteamboss@gmail.com>
2024-09-24 17:09:34 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
 fun main() {
-    println("Hello World!")
+
+    val b = ToDec("1010")
+    MyNum(b).toDec()
+
 }
+
+interface Dec{
+    fun toDec()
+}
+
+class ToDec(val num: String): Dec{
+    override fun toDec() {
+        println("Число в бинарной системе счисления ${num.toLong(2)}")
+    }
+}
+
+class MyNum(num: Dec):Dec by num
